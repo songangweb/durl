1a8ce87f34911e41f8a58fa6972294b81efb28eb
leon <13240325712@163.com>
2021-11-11 10:17:25 +0800

    backend 模块创建

---
+++ app/exec/backend/routers/router.go
@@ -0,0 +1,26 @@
+package routers
+
+import (
+	"durl/app/exec/backend/controllers"
+	"github.com/beego/beego/v2/server/web"
+)
+
+// RouterHandler 路由跳转
+func RouterHandler() {
+
+	// openApi初始化
+	controllers.InitCon()
+
+	// 获取xsrfToken
+	web.Router("/xsrf-token", &controllers.Controller{}, "get:GetXsrfToken")
+
+	// 设置短链
+	web.Router("/url", &controllers.Controller{}, "post:SetShortUrl")
+
+	// 修改短链
+	web.Router("/url", &controllers.Controller{}, "put:UpdateShortUrl")
+
+	// 删除短链
+	web.Router("/url", &controllers.Controller{}, "delete:DelShortKey")
+
+}
