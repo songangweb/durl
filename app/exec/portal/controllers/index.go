2bded5a34d99f9165b0279fb2f3a32107c626296
songang <songangweb@foxmail.com>
2021-12-20 15:29:35 +0800

    测试百度统计

--- app/exec/portal/controllers/index.go
+++ app/exec/portal/controllers/index.go
@@ -10,7 +10,8 @@ func (c *Controller) Index() {
 	c.Data["xsrf_token"] = c.XSRFToken()
 
 	// 百度统计key
-	sConf, _ := config.String("Baidu")
+	runmode, _ := config.String("runmode")
+	sConf, _ := config.String(runmode+ "::Baidu")
 	if sConf != "" {
 		c.Data["Statistical_Baidu_Key"] = sConf
 	}
